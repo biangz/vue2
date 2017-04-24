d372f69c176ee40b4d6f7ccd557b845125e2aa36
bianguanzhou <guanzhou@zuoshu.com>
2017-04-24 16:50:16 +0800

    vue-cli项目学习

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <!-- <tag></tag> -->
+    <router-view></router-view>
+    <!-- <template></template> -->
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app'
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Tag.vue
@@ -0,0 +1,37 @@
+<template>
+<div class="tag-wrap">
+  <span>标签：</span>
+  <input type="text" name="tag" id="tag" value="" v-model="val" />
+  <button type="button" name="button" v-on:click="add"></button>
+  <ul>
+    <li v-for="item in cont">{{item}}</li>
+  </ul>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'tag',
+  data () {
+    return {
+      cont: ['1', '2'],
+      val: ''
+    }
+  },
+  methods: {
+    add: function () {
+      let _val = this.val
+      if (_val.length === 0 || this.cont.indexOf(_val) > -1) {
+        return
+      }
+      this.cont.push(this.val)
+    }
+  },
+  props: {
+    width: {
+      type: String,
+      default: 'auto'
+    }
+  }
+}
+</script>
